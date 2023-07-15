4c77b32acd022d0941060cd4f18e8f41d70f754c
Mustafa Mahrous <m.qamaruddin.89@gmail.com>
2023-07-15 11:19:58 +0200

    Add unit tests: add enrich service test

--- enricher/src/test/java/enrichers/EnrichServiceTest.java
+++ enricher/src/test/java/enrichers/EnrichServiceTest.java
@@ -8,14 +8,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EnrichServiceTest {
@@ -40,7 +45,7 @@ class EnrichServiceTest {
             "https://subdomain.example2.com", 654, "2022-01-05T00:00:00Z", 456
         )
     );
-    Mockito.when(
+    when(
         domainRegistrationClient.queryDomainInfos(
             List.of("example.com", "example2.com")
         )
@@ -77,5 +82,86 @@ class EnrichServiceTest {
     assertEquals(456, enrichedClassification2.getLogic());
     assertNotNull(enrichedClassification2.getDomainAgeInDays());
     assertEquals(3, enrichedClassification2.getDomainAgeInDays());
+  }
+
+  @Test
+  void testNoMatchingDomains() {
+    // Given
+    List<ClassificationDecision> classificationDecisionList = List.of(
+        new ClassificationDecision(
+            "https://example.com", 321, "2022-01-03T00:00:00Z", 123
+        ),
+        new ClassificationDecision(
+            "https://example3.com", 321, "2022-01-03T00:00:00Z", 123
+        )
+    );
+    when(domainRegistrationClient.queryDomainInfos(
+        List.of("example.com", "example3.com")
+    )).thenReturn(
+        Map.of(
+            "example.com", new DomainInfo("2022-01-01T00:00:00Z", "", ""),
+            // No matching domain info for "example3.com"
+            "example4.com", new DomainInfo("2022-01-02T00:00:00Z", "", "")
+        )
+    );
+
+    // When
+    List<EnrichedClassification> enrichedClassificationList = enrichService.enrichClassifications(
+        classificationDecisionList
+    );
+
+    // Then
+    assertEquals(2, enrichedClassificationList.size());
+
+    // Check the values of the first EnrichedClassification object (successful enrichment)
+    EnrichedClassification enrichedClassification1 = enrichedClassificationList.get(0);
+    assertEquals("https://example.com", enrichedClassification1.getUrl());
+    assertEquals("example.com", enrichedClassification1.getDomainName());
+    assertEquals(321, enrichedClassification1.getClassification());
+    assertEquals("2022-01-03T00:00:00Z", enrichedClassification1.getCreated());
+    assertEquals(123, enrichedClassification1.getLogic());
+    assertNotNull(enrichedClassification1.getDomainAgeInDays());
+
+    // Check the values of the second EnrichedClassification object (no matching domain)
+    EnrichedClassification enrichedClassification2 = enrichedClassificationList.get(1);
+    assertEquals("https://example3.com", enrichedClassification2.getUrl());
+    assertEquals(null, enrichedClassification2.getDomainName());
+    assertEquals(321, enrichedClassification2.getClassification());
+    assertEquals("2022-01-03T00:00:00Z", enrichedClassification2.getCreated());
+    assertEquals(123, enrichedClassification2.getLogic());
+    assertNull(enrichedClassification2.getDomainAgeInDays());
+
+    // Verify the interactions with domainRegistrationClient
+    verify(domainRegistrationClient).queryDomainInfos(List.of("example.com", "example3.com"));
+  }
+
+  @Test
+  void testEmptyClassificationList() {
+    // Given
+    List<ClassificationDecision> classificationDecisionList = new ArrayList<>();
+
+    // When
+    List<EnrichedClassification> enrichedClassificationList = enrichService.enrichClassifications(
+        classificationDecisionList
+    );
+
+    // Then
+    assertTrue(enrichedClassificationList.isEmpty());
+    verifyNoInteractions(domainRegistrationClient);
+  }
+
+  @Test
+  void testNullClassificationList() {
+    // Given
+    List<ClassificationDecision> classificationDecisionList = null;
+
+    // When
+    List<EnrichedClassification> enrichedClassificationList = enrichService.enrichClassifications(
+        classificationDecisionList
+    );
+
+    // Then
+    assertTrue(enrichedClassificationList.isEmpty());
+    verifyNoInteractions(domainRegistrationClient);
   }
 }
