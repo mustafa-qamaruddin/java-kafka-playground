a5629ce4121095fb2311e7f19bd6062d5ab7660b
Mustafa Mahrous <m.qamaruddin.89@gmail.com>
2023-07-15 16:50:16 +0200

    Add Enrich Service Test

--- enricher/src/test/java/qubits/enrichers/EnrichServiceTest.java
+++ enricher/src/test/java/qubits/enrichers/EnrichServiceTest.java
@@ -82,6 +82,9 @@ class EnrichServiceTest {
     assertEquals(456, enrichedClassification2.getLogic());
     assertNotNull(enrichedClassification2.getDomainAgeInDays());
     assertEquals(3, enrichedClassification2.getDomainAgeInDays());
+
+    // Verify the interactions with domainRegistrationClient
+    verify(domainRegistrationClient).queryDomainInfos(List.of("example.com", "example2.com"));
   }
 
   @Test
@@ -89,19 +92,17 @@ class EnrichServiceTest {
     // Given
     List<ClassificationDecision> classificationDecisionList = List.of(
         new ClassificationDecision(
-            "https://example.com", 321, "2022-01-03T00:00:00Z", 123
+            "https://example3.com", 321, "2022-01-03T00:00:00Z", 123
         ),
         new ClassificationDecision(
-            "https://example3.com", 321, "2022-01-03T00:00:00Z", 123
+            "https://example4.com", 654, "2022-01-04T00:00:00Z", 456
         )
     );
     when(domainRegistrationClient.queryDomainInfos(
-        List.of("example.com", "example3.com")
+        List.of("example3.com", "example4.com")
     )).thenReturn(
         Map.of(
-            "example.com", new DomainInfo("2022-01-01T00:00:00Z", "", ""),
-            // No matching domain info for "example3.com"
-            "example4.com", new DomainInfo("2022-01-02T00:00:00Z", "", "")
+            "example4.com", new DomainInfo("2022-01-01T00:00:00Z", "", "")
         )
     );
 
@@ -115,24 +116,25 @@ class EnrichServiceTest {
 
     // Check the values of the first EnrichedClassification object (successful enrichment)
     EnrichedClassification enrichedClassification1 = enrichedClassificationList.get(0);
-    assertEquals("https://example.com", enrichedClassification1.getUrl());
-    assertEquals("example.com", enrichedClassification1.getDomainName());
+    assertEquals("https://example3.com", enrichedClassification1.getUrl());
+    assertEquals("example3.com", enrichedClassification1.getDomainName());
     assertEquals(321, enrichedClassification1.getClassification());
     assertEquals("2022-01-03T00:00:00Z", enrichedClassification1.getCreated());
     assertEquals(123, enrichedClassification1.getLogic());
-    assertNotNull(enrichedClassification1.getDomainAgeInDays());
+    assertNull(enrichedClassification1.getDomainAgeInDays());
 
     // Check the values of the second EnrichedClassification object (no matching domain)
     EnrichedClassification enrichedClassification2 = enrichedClassificationList.get(1);
-    assertEquals("https://example3.com", enrichedClassification2.getUrl());
-    assertNull(enrichedClassification2.getDomainName());
-    assertEquals(321, enrichedClassification2.getClassification());
-    assertEquals("2022-01-03T00:00:00Z", enrichedClassification2.getCreated());
-    assertEquals(123, enrichedClassification2.getLogic());
-    assertNull(enrichedClassification2.getDomainAgeInDays());
+    assertEquals("https://example4.com", enrichedClassification2.getUrl());
+    assertEquals("example4.com", enrichedClassification2.getDomainName());
+    assertEquals(654, enrichedClassification2.getClassification());
+    assertEquals("2022-01-04T00:00:00Z", enrichedClassification2.getCreated());
+    assertEquals(456, enrichedClassification2.getLogic());
+    assertNotNull(enrichedClassification2.getDomainAgeInDays());
+    assertEquals(3, enrichedClassification2.getDomainAgeInDays());
 
     // Verify the interactions with domainRegistrationClient
-    verify(domainRegistrationClient).queryDomainInfos(List.of("example.com", "example3.com"));
+    verify(domainRegistrationClient).queryDomainInfos(List.of("example3.com", "example4.com"));
   }
 
   @Test
